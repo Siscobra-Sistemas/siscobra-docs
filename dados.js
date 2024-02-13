e52a095421f514f1e66c2fc1efaf8eacda6c6285
Guilherme Luiz Lange <gui.luizlange@gmail.com>
2024-02-13 14:10:54 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,63 +2,63 @@ const api = {
   Manager: [
     {
       name: "WSAssessoria",
-      uri: "api/manager/wsassessoria",
+      uri: "apis/manager/wsassessoria",
       description:
         "Serviço SOAP WSAssessorias para permitir a manutenção dos dados de cobrança entre credor e Assessorias",
     },
     {
       name: "WSAssessoria Internacional",
-      uri: "api/manager/wsassessoriainternacional",
+      uri: "apis/manager/wsassessoriainternacional",
       description:
         "Serviço SOAP WSAssessorias para permitir a manutenção dos dados de cobrança entre credor e Assessorias",
     },
     {
       name: "Siscobra WS",
-      uri: "api/manager/siscobraws",
+      uri: "apis/manager/siscobraws",
       description: "Serviço SOAP SiscobraWS para portal de autonegociações",
     },
     {
       name: "WSLote",
-      uri: "api/manager/wslote",
+      uri: "apis/manager/wslote",
       description: "Importação de Remessa via Rest",
     },
     {
       name: "WS Usuário",
-      uri: "api/manager/wsusuario",
+      uri: "apis/manager/wsusuario",
       description: "Integração com a API para gestão de usuário",
     },
     {
       name: "WS Fraude",
-      uri: "api/manager/wsfraude",
+      uri: "apis/manager/wsfraude",
       description: "API para gerenciamento de fraudes",
     },
   ],
   Evolution: [
     {
       name: "Siscobra WS",
-      uri: "api/evolution/siscobraws",
+      uri: "apis/evolution/siscobraws",
       description: "Serviço SOAP SiscobraWS para portal de autonegociações",
     },
     {
       name: "Boleto API",
-      uri: "api/evolution/boletoapi",
+      uri: "apis/evolution/boletoapi",
       description: "Integração via API para geração de boletos",
     },
     {
       name: "Siscobra API",
-      uri: "api/evolution/siscobraapi",
+      uri: "apis/evolution/siscobraapi",
       description: "Integração disponíveis no WebService da Siscobra",
     },
   ],
   Pay: [
     {
       name: "Pay Boleto",
-      uri: "api/pay/payboleto",
+      uri: "apis/pay/payboleto",
       description: "Integração via API com a plataforma Pay Boleto",
     },
     {
       name: "Pay Cartão",
-      uri: "api/pay/paycartao",
+      uri: "apis/pay/paycartao",
       description: "Integração via API com a plataforma Pay Cartão",
     },
   ],
